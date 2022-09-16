89b4409230613289bfc874685d6e7b9486271cbb
polvinh0 <ieiel_4@hotmail.com>
2022-09-16 14:17:26 -0300

    upload um

---
+++ index.js
@@ -0,0 +1,22 @@
+/** 
+ * -> tipos primitivos
+ * -> variaveis do javascrypt
+ * -> Operadires matematicos do javascript
+*/
+
+// serve para escrever mensagens no console
+
+// string -> textos
+// number -> numeros
+// boolean -> verdadeiro/falso
+
+console.log('Hello world')
+console.log('5')
+console.log(46.76)
+console.log(true)
+console.log(false)
+
+//variaveis
+
+let mensagem = "minha menasgem"
+console.log(mensagem)
