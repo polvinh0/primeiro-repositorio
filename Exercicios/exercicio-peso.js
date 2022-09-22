c5c2d718250fef101e12dc4b0a4ce6f38443c848
polvinh0 <ieiel_4@hotmail.com>
2022-09-21 23:18:31 -0300

    exercicios com operadores logicos

--- Exercicios/exercicio-peso.js
+++ Exercicios/exercicio-peso.js
@@ -10,8 +10,8 @@ console.log(sexo)
 if (sexo == "M" || sexo == "m") {
     const pesoIdeal = (72.7 * altura)-58
     alert(`Seu peso ideal é: ${pesoIdeal.toFixed(2)}KG`)
-}else if ((sexo == "F" || sexo == "f")) {
+}else if((sexo == "F" || sexo == "f")) {
     const pesoIdeal = (62.1 * altura)-44.7
     alert(`Seu peso ideal é: ${pesoIdeal.toFixed(2)}KG`)
 
-}
+}else {alert(`Opção invalida`)} 
