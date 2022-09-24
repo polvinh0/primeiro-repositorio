e9a8dd09a9232333e1079a300898c110539d0347
polvinh0 <ieiel_4@hotmail.com>
2022-09-23 22:57:44 -0300

    exercicios for

---
+++ arrays.js
@@ -0,0 +1,16 @@
+/* arrays ou vetores é um tipo especial que te permite armazernar diversos valores 
+em apenas um local*/
+
+
+// * CRIANDO UM ARRAY *
+
+// let arr = new Array()
+// let arr2 = new Array(5)
+
+let arr = [1,'teste',3,4,5,6,7,8,9,0]
+console.log(arr[6])
+arr[6] = 'Outro valor'
+console.log(arr[6])
+console.log(arr[8]) 
+arr[8] = 'Agora tem 8'
+console.log(arr[8])
